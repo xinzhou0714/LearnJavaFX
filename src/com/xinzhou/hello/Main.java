30cb67127ad4bef9fe3b14131adb01602e3a02fe
xinzhou0714 <zhouxin0714@gmail.com>
2021-12-28 16:05:01 +0100

    002(P1): basic JavaFX structure

--- src/com/xinzhou/hello/Main.java
+++ src/com/xinzhou/hello/Main.java
@@ -1,6 +1,9 @@
 package com.xinzhou.hello;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -12,6 +15,13 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws Exception {
 
+        Label label = new Label("Hallo Word");
+        BorderPane pane =new BorderPane(label);
+
+        Scene scene = new Scene(pane ,600,480);
+
+
+        stage.setScene(scene);
         stage.setTitle("JavaFX Demo");
         stage.show();
 
